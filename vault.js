87da338f03d9f68c9ae4a760f739dca9563b890e
berzyvonne <berzyvonne@gmail.com>
2024-01-30 14:17:51 -0500

    updated html and js

--- vault.js
+++ vault.js
@@ -1,11 +1,30 @@
 // Pseudocode:
-// 1. I will create a string with directions to unlock the vault.
-// 2. I will create three arithmetic operators, each equalling one of the three numbers in the combination.
-//   -- let num1 = 5 * 2;  let num2 = 50 - 10; let num3 = 30 + 9
-// 3. I'll add comments throughout the code to explain my steps.
-// 4. I'll create a pop-up dialog to display the vault code.
-// 5. I'll submit my assignment.
-
-const str = 'You' + ' ' + 'have' + ' ' + 'received' + ' ' + 'this' + ' ' + 'message' + ' ' + 'because' + ' ' + 'you' + ''
+// 1. Create a string with directions to unlock the vault.
+// 2. Create three arithmetic operators, each equalling one of the three numbers in the combination.
+//   -- const num1 = 5 * 2;  const num2 = 50 - 10; const num3 = 30 + 9
+// 3. Create a pop-up dialog to display the directions and each number of the vault combination using alert().
+// 4. Add comments throughout the code to explain my steps.
+// 5. Commit to GitHub.
 
 
+// Directions to unlock the vault
+const str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Number 1 of the combination
+const num1 = 5*2;
+
+// Number 2 of the combination
+const num2 = 50-10;
+
+// Number 3 of the combination
+const num3 = 30+9;
+
+
+// Alerts for the above
+alert(str1)
+
+alert(num1)
+
+alert(num2)
+
+alert(num3)
