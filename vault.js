18c6ecf5f713fe00c21559596c802d62fc90cf86
berzyvonne <berzyvonne@gmail.com>
2024-01-29 23:28:32 -0500

    add html and js

---
+++ vault.js
@@ -0,0 +1,11 @@
+// Pseudocode:
+// 1. I will create a string with directions to unlock the vault.
+// 2. I will create three arithmetic operators, each equalling one of the three numbers in the combination.
+//   -- let num1 = 5 * 2;  let num2 = 50 - 10; let num3 = 30 + 9
+// 3. I'll add comments throughout the code to explain my steps.
+// 4. I'll create a pop-up dialog to display the vault code.
+// 5. I'll submit my assignment.
+
+const str = 'You' + ' ' + 'have' + ' ' + 'received' + ' ' + 'this' + ' ' + 'message' + ' ' + 'because' + ' ' + 'you' + ''
+
+
